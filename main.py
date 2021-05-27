82aca272b3f78101545bbbbed80134bbad5ae94b
Diego Duarte <projectdcd@gmail.com>
2021-05-26 21:31:44 -0500

    trying new config again

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, HTTPException
 from pony.orm import *
 from pydantic import BaseModel
 import os
-import uvicorn
 
 db = Database()
 db.bind(provider='mysql', host=os.environ['mysqlhost'], user=os.environ['mysqluser'], 
@@ -42,7 +41,3 @@ def get_one_country(country_code: str, date: str):
         response['Deaths'] = i.deaths
         response['Date'] = i.creationDate
     return response
-    
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
